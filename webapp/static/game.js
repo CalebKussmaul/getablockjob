0d34a6c58969bb0a5bb21287f194bd1b8bf88448
Caleb Kussmaul <calebwk@gmail.com>
2017-11-04 14:55:58 -0400

    added timers and fancy shit yo

--- webapp/static/game.js
+++ webapp/static/game.js
@@ -1,34 +1,23 @@
 var game_data = {
-    blocks: [
-        {
-            type: "basic",
-            color: "#F00",
-            damage: .5,
-            x: 10,
-            y: 10
-        }
-    ]
+    blocks: []
 };
 var game_delay = 1000;
 var canvas_offset_x = 0;
 var canvas_offset_y = 0;
 var canvas_zoom = 10;
-var selected_block = {
-    type: "basic",
-    color: "#F00",
-    damage: .5,
-    x: 0,
-    y: 0
-};
+var selected_block ={};
+setColor("#F00");
 var xhover = 0;
 var yhover = 0;
 
-var dragging;
+var dragging = null;
+
+var next_block = Date.now()+1000;
 
 $(document).ready(function () {
     var canvas = $("#game");
-    canvas[0].width = 500;
-    canvas[0].height = 500;
+    canvas[0].width = $(document).width();
+    canvas[0].height = $(document).height();
     var ctx = canvas[0].getContext('2d');
     ctx.imageSmoothingEnabled = false;
     ctx.translate(.5, .5);
@@ -36,26 +25,38 @@ $(document).ready(function () {
     console.log("this happens once");
 
 
-    // window.setInterval(function () {
-    //
-    //     $.ajax({
-    //         url:"/gamedata.json"
-    //     }).done(function (data) {
-    //         game_data = JSON.parse(data);
-    //         drawGame();
-    //     });
-    // }, game_delay);
+    window.setInterval(function () {
+        updateTimeout();
+
+        // $.ajax({
+        //     url:"/gamedata.json"
+        // }).done(function (data) {
+        //     game_data = JSON.parse(data);
+        //     drawGame();
+        // });
+    }, game_delay);
     drawGame(canvas, ctx);
     
     canvas.click(function (e) {
-        if(selected_block.type === "pan") {
-
+        if(selected_block.type === "pan" || !cooldownReady()) {
             return;
         }
+
         var elm = $(this);
-        selected_block.x = Math.round((e.pageX - elm.offset().left)/canvas_zoom) - canvas_offset_x;
-        selected_block.y = Math.round((e.pageY - elm.offset().top)/canvas_zoom)  - canvas_offset_y;
-        game_data.blocks.push(Object.assign({}, selected_block));
+        selected_block.x = Math.round((e.pageX - elm.offset().left - canvas_offset_x)/canvas_zoom);
+        selected_block.y = Math.round((e.pageY - elm.offset().top  - canvas_offset_y)/canvas_zoom);
+
+
+        var b2p = Object.assign({}, selected_block);
+        for(var i = 0; i < game_data.blocks.length; i++) {
+            var b = game_data.blocks[i];
+            if(b.x === b2p.x && b.y === b2p.y && b.color === b2p.color) {
+                b2p.health = Math.floor(b.health+1);
+                game_data.blocks.splice(i);
+                break;
+            }
+        }
+        game_data.blocks.push(b2p);
         drawGame(canvas, ctx);
     });
 
@@ -67,11 +68,11 @@ $(document).ready(function () {
     }).mousemove(function (e) {
         if(dragging !==null) {
 
-            var offset_x = (e.pageX - dragging.x)/canvas_zoom;
-            var offset_y = (e.pageY - dragging.y)/canvas_zoom;
+            var offset_x = (e.pageX - dragging.x);
+            var offset_y = (e.pageY - dragging.y);
             canvas_offset_x+=offset_x;
             canvas_offset_y+=offset_y;
-            ctx.translate(offset_x, offset_y);
+            ctx.translate(offset_x/canvas_zoom, offset_y/canvas_zoom);
             dragging = {x:e.pageX, y:e.pageY};
             drawGame(canvas, ctx);
 
@@ -85,8 +86,8 @@ $(document).ready(function () {
 
     canvas.mousemove(function (e) {
         var elm = $(this);
-        xhover = Math.round((e.pageX - elm.offset().left)/canvas_zoom) - canvas_offset_x;
-        yhover = Math.round((e.pageY - elm.offset().top)/canvas_zoom)  - canvas_offset_y;
+        xhover = Math.round((e.pageX - elm.offset().left - canvas_offset_x)/canvas_zoom);
+        yhover = Math.round((e.pageY - elm.offset().top  - canvas_offset_y)/canvas_zoom);
         drawGame(canvas, ctx);
     });
 
@@ -119,13 +120,33 @@ function setBlock(block) {
 }
 
 function setColor(c) {
+
     setBlock({
         type: "basic",
         color: c,
-        damage: .5,
+        health: 0,
+        cooldown: 0,
         x: 0,
         y: 0
     });
+}
+
+function cooldownReady() {
+    return next_block - Date.now() <= 0;
+}
+
+function updateTimeout() {
+    var secsRemaining = Math.round((next_block - Date.now())/1000);
+
+    if(secsRemaining <= 0) {
+        $("#countdown").text("0:00");
+    } else {
+        var mins = Math.round(secsRemaining/60);
+        var secs = secsRemaining % 60;
+        if(secs < 10)
+            secs = "0"+secs;
+        $("#countdown").text(mins+":"+secs);
+    }
 }
 
 function drawGame(canvas, ctx) {
@@ -143,6 +164,8 @@ function drawGame(canvas, ctx) {
     for (var i = 0; i < game_data.blocks.length; i++) {
         var block = game_data.blocks[i];
         ctx.fillStyle = block.color;
+        ctx.fillRect(block.x, block.y, 1, 1);
+        ctx.fillStyle = "#000" + Math.floor(Math.min(9, block.health));
         ctx.fillRect(block.x, block.y, 1, 1);
     }
 
