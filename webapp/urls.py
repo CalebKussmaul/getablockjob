25cef74a0e51c6f3421cbbdbabc54ebd404f947f
Caleb Kussmaul <calebwk@gmail.com>
2017-11-04 04:08:04 -0400

    started game stuff

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^signup/$', views.signup, name='home'),
     url(r'^game/$', views.game, name='game'),
+    url(r'^gamedata.json$', views.gamedata, name='gamedata'),
+
 ]
