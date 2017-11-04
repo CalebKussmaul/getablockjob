f51196763071ad133819757e9d29bea342f6366d
Chenhao Huang <chenhaohuang96@gmail.com>
2017-11-04 03:46:23 -0400

    api first impl
    place/delete block, block, get board

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Block():
+    def getColor(self):
+        return self.color
+
+    def getCord(self):
+        return self.cord
+
+    def getHealth(self):
+        return self.health
+
+    def setHealth(self, health):
+        self.health = health
+
+    def __init__(self, color, cord):
+        self.color = color
+        self.cord = cord
+        self.health = 1.0
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,18 @@
+from django.http import QueryDict
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+import json
+from .models import Block
+
+CORD = 'cord'
+COLOR = 'color'
+
+
+def initialize_board(mode):
+    global board
+    board = {}
 
 
 def signup(request):
@@ -17,3 +28,44 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def place_block(request):
+    if request.method == 'GET':
+        response = request.GET
+        if response.get(CORD) is not None and response.get(COLOR) is not None:
+            cord = response.get(CORD)
+            color = response.get(COLOR)
+            success = False
+            if cord not in board:
+                board[cord] = Block(color, cord)
+                success = color
+            elif board[cord].getColor() == color:
+                board[cord].setHealth(board[cord].getHeath() + 1.0)
+                success = color
+            elif board[cord].getHeath() <= 1.0:
+                board[cord] = Block(color, cord)
+                success = color
+            else:
+                board[cord].setHealth(board[cord].getHeath() - 1.0)
+                success = board[cord].getColor
+            return success
+    return False
+
+
+def delete_block(request):
+    if request.method == 'GET':
+        response = request.GET
+        cord = response.get(CORD)
+        if cord is not None:
+            if cord in board:
+                color = board[cord]
+                board.pop(cord, None)
+                return color
+    return False
+
+
+def get_board(request):
+    if request.method == 'GET':
+        return json.dump(board)
+    return False
