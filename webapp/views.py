7d8b91603244db3c0c511f558ccc88afcd7b68ba
Caleb Kussmaul <calebwk@gmail.com>
2017-11-04 03:04:02 -0400

    began work on frontend

--- webapp/views.py
+++ webapp/views.py
@@ -17,3 +17,8 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def game(request):
+
+    return render(request, "game.html")
